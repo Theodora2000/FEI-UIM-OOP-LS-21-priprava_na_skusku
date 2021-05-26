a5edf41601b2645c51c5f22b2d62b6e6de367132
Theodora2000 <xsimont@stuba.sk>
2021-05-26 15:52:15 +0200

    6.e/6l

--- src/sk/stuba/uim/fei/oop/Main6.java
+++ src/sk/stuba/uim/fei/oop/Main6.java
@@ -5,16 +5,6 @@ import java.awt.*;
 
 public class Main6 {
     public static void main(String args[]){
-        JFrame f = new JFrame();
-        f.setSize(300,300);
-
-        JPanel p = new JPanel();
-        MyButton b1 = new MyButton("Click");
-        p.add(b1);
-        JButton iny = new JButton("Click right");
-        iny.addActionListener(b1);
-        p.add(iny);
-        f.add(p);
-        f.setVisible(true);
+        new TestFrame("Novy frame");
     }
 }
