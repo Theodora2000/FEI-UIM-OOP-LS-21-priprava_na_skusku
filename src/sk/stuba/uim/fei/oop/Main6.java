ad9cb33a34ce6d06d3e33ea8c3978e16e87cc824
Theodora2000 <xsimont@stuba.sk>
2021-05-26 14:51:42 +0200

    2.e/6l

--- src/sk/stuba/uim/fei/oop/Main6.java
+++ src/sk/stuba/uim/fei/oop/Main6.java
@@ -6,13 +6,14 @@ import java.awt.*;
 public class Main6 {
     public static void main(String args[]){
         JFrame f = new JFrame();
-        f.setSize(400,400);
-        f.setLayout(new BorderLayout());
-        f.add(BorderLayout.CENTER,new Canvas5());
-        JPanel p = new JPanel();
-        p.add(new JButton("Change"));
-        p.add(new JButton("Close"));
-        f.add(BorderLayout.SOUTH, p);//juh
+        f.setLayout(new GridLayout(0,2));//ked dame 0, tak to bude tolko riadkov kolko potrebujeme
+        f.add(new JLabel("riadok 1, stlpec1"));
+        f.add(new JLabel("riadok 1, stlpec2"));
+        f.add(new JLabel("riadok 2, stlpec1"));
+        f.add(new JLabel("riadok 2, stlpec2"));
+        f.add(new JLabel("riadok 3, stlpec1"));
+        f.pack();//spakuje to do najmensieho pekneho tvaru
+
         f.setVisible(true);
     }
 }
