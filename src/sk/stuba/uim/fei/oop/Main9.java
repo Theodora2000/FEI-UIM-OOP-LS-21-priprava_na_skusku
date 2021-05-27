9f7fb0ed5f29074cd96e31fec9e8092ec5aa6978
Theodora2000 <xsimont@stuba.sk>
2021-05-27 14:21:33 +0200

    4.e/9l FileReader, Open, Save as

--- src/sk/stuba/uim/fei/oop/Main9.java
+++ src/sk/stuba/uim/fei/oop/Main9.java
@@ -4,19 +4,6 @@ import java.io.File;
 
 public class Main9 {
     public static void main(String args[]){
-
-        File dir = new File("C:\\Users\\WIN10\\Desktop\\FEI\\4.semeter\\OOP\\Priprava na skusku\\FEI-UIM-OOP-LS-21-priprava_na_skusku\\src\\sk\\stuba\\uim\\fei\\oop");
-        String[] list = dir.list();
-        for(int i=0;i<list.length;i++){
-            System.out.println(list[i]);
-        }
-        System.out.println("---------------");
-        for(int i=0;i<list.length;i++){
-            if(list[i].endsWith(".java"));{
-                File tempor = new File(dir, list[i]);
-                long length = tempor.length();
-                System.out.println(list[i] + " [" + length +"]");
-            }
-        }
+        NasMalyEditor9 f = new NasMalyEditor9();
     }
 }
